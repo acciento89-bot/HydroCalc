65cb747c208c652fe489c1ea0dc8f427d51563f0
Kamilunavo <acciento89@gmail.com>
2026-09-13 16:25:00 +0200

    Use light status icons on dark HydroCalc UI

--- android/app/src/main/java/de/kamilunavo/hydrocalc/MainActivity.kt
+++ android/app/src/main/java/de/kamilunavo/hydrocalc/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -51,6 +52,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
         val showBillingStatus = shouldShowBillingStatus(
             intent.getBooleanExtra(storeScreenshotsExtra(packageName), false),
         )
