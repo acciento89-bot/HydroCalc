d65861611bf9388bcb8f49a0817fb289effb5534
Kamilunavo <acciento89@gmail.com>
2026-09-13 15:38:03 +0200

    Test namespaced store screenshot extra

--- android/app/src/test/java/de/kamilunavo/hydrocalc/StoreScreenshotModeTest.kt
+++ android/app/src/test/java/de/kamilunavo/hydrocalc/StoreScreenshotModeTest.kt
@@ -1,5 +1,6 @@
 package de.kamilunavo.hydrocalc
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -7,6 +8,7 @@ import org.junit.Test
 class StoreScreenshotModeTest {
     @Test
     fun hidesBillingStatusOnlyForStoreScreenshots() {
+        assertEquals("de.kamilunavo.hydrocalc.STORE_SCREENSHOTS", storeScreenshotsExtra("de.kamilunavo.hydrocalc"))
         assertFalse(shouldShowBillingStatus(storeScreenshots = true))
         assertTrue(shouldShowBillingStatus(storeScreenshots = false))
     }
